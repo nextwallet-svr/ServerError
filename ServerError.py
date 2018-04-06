a7a4073f2e5ad1bc7963c876d8877ac92483ad90
rockercheng <8350088@qq.com>
2018-04-06 17:04:45 +0800

    update error

--- ServerError.py
+++ ServerError.py
@@ -1,19 +1,23 @@
 import json
 
-server_error_dict = {}
+def load_error_desc(filename):
+    error_dict = {}
 
-def load_error_desc():
-	#载入主网合约abi
-    with open("serverError.json") as server_error_file:
-        server_error_dict = json.load(server_error_file)
+    #载入主网合约abi
+    with open(filename) as server_error_file:
+        error_dict = json.load(server_error_file)
+    return error_dict
 
-def build_error_result(error_tag):
-	if not server_error_dict.__contains__(error_tag):
-		code = server_error_dict["SERVER_INTERNAL_ERR"]["code"]
-		desc = server_error_dict["SERVER_INTERNAL_ERR"]["desc"]
-	else:
-		code = server_error_dict[error_tag]["code"]
-		desc = server_error_dict[error_tag]["desc"]
-	return { "retcode": code, "message" : desc }
+def build_error_result(error_dict, error_tag):
+    if not error_dict.__contains__(error_tag):
+        print(error_dict)
+        print(error_dict["SERVER_INTERNAL_ERR"], error_tag)
 
+        code = error_dict["SERVER_INTERNAL_ERR"]["code"]
+        desc = error_dict["SERVER_INTERNAL_ERR"]["desc"]
+    else:
+        print(error_dict[error_tag], error_tag)
 
+        code = error_dict[error_tag]["code"]
+        desc = error_dict[error_tag]["desc"]
+    return { "retcode": code, "message" : desc }
